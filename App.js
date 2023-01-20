4081bef5d9740ce2886d339ef970898114e2230b
QuackDuster <pablo.jimenezdev@gmail.com>
2023-01-19 18:32:25 -0600

    Finished the design for the Login screen, and creating new styles
    Co-authored-by: victordoom <23017227+victordoom@users.noreply.github.com>
    Co-authored-by: Leonardo-Aguirre-Ponce <Leonardo-Aguirre-Ponce@users.noreply.github.com>
    Co-authored-by: Rodrigo Aparicio <Zuack55@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StyleSheet, View } from "react-native";
-import SignUp from "./src/views/SignUp";
-import Constants from "expo-constants";
+import { StyleSheet, View } from 'react-native';
+import SignUp from './src/views/SignUp';
+import Login from './src/views/Login';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <SignUp />
+      <Login />
     </View>
   );
 }
@@ -14,8 +15,8 @@ const styles = StyleSheet.create({
   container: {
     marginTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
