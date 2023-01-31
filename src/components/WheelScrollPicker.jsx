fa9d70f4958c349bf40aa8157cbe424a524ddbc0
QuackDuster <pablo.jimenezdev@gmail.com>
2023-01-30 18:57:42 -0600

    Designed and created the component of ScrollPicker

---
+++ src/components/WheelScrollPicker.jsx
@@ -0,0 +1,39 @@
+import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import WheelPicker from 'react-native-wheely';
+import { COLORS } from '../theme/colors';
+
+export default class WheelScrollPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: '',
+      selectedIndex: 0,
+    };
+  }
+  render() {
+    return (
+      <WheelPicker
+        selectedIndicatorStyle={{
+          height: 2,
+          width: 300,
+          alignContent: 'center',
+          alignSelf: 'center',
+          alignSelfContent: 'center',
+          borderRadius: 0,
+          borderTopColor: COLORS.LIGHT_GRAY,
+          borderBottomColor: COLORS.LIGHT_GRAY,
+          borderTopWidth: 1,
+          borderBottomWidth: 1,
+        }}
+        itemTextStyle={{ color: COLORS.BLACK_GRAY, fontSize: 30, fontWeight: 'bold' }}
+        containerStyle={{ fontSize: 30, height: 280 }}
+        itemHeight={40}
+        visibleRest={3}
+        selectedIndex={this.state.selectedIndex}
+        options={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
+        onChange={(index) => this.setState({ ...this.props.state, selectedIndex: index })}
+      />
+    );
+  }
+}
