1a0f72aaca2b9e724b4af70a4fd43be8072b504a
QuackDuster <pablo.jimenezdev@gmail.com>
2023-02-07 16:16:41 -0600

    Displayed the information of the collection in the MyFlights screen
    Co-authored-by: victordoom <23017227+victordoom@users.noreply.github.com>
    Co-authored-by: Leonardo-Aguirre-Ponce <Leonardo-Aguirre-Ponce@users.noreply.github.com>
    Co-authored-by: Rodrigo Aparicio <Zuack55@users.noreply.github.com>

--- src/views/MyFlights.jsx
+++ src/views/MyFlights.jsx
@@ -1,105 +1,66 @@
 import React, { Component } from 'react';
-import { ScrollView, Text, View } from 'react-native';
-import { FlatList } from 'react-native-web';
+import { ScrollView, Text, View, FlatList } from 'react-native';
 import CardFlight from '../components/CardFlight';
 import FloatingButton from '../components/FloatingButton';
 import { StyleFloatingButton } from '../theme/StyleFloatingButton';
 import { styleComponents } from '../theme/StyleSignUp';
 import { stylesContainer } from '../theme/StyleCardFlight';
+import { auth, db } from '../db/firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 export default class MyFlights extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      info: {
+        data: [],
+        id: '',
+      },
+    };
+  }
+
+  async componentDidMount() {
+    await this.getItem();
+  }
+  getItem = async () => {
+    try {
+      const q = collection(db, 'users', auth.currentUser.uid, 'flights');
+
+      const querySnapshot = await getDocs(q);
+
+      let newarray = [];
+
+      querySnapshot.forEach((doc) => {
+        let itemArray = {
+          data: doc.data(),
+          id: doc.id,
+        };
+        newarray = [...newarray, itemArray];
+      });
+
+      this.setState((prevState) => {
+        let info = Object.assign({}, prevState.info);
+        info = newarray;
+        return { info };
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   render() {
     return (
       <View style={stylesContainer.container}>
         <View style={StyleFloatingButton.container}>
           <Text style={styleComponents.StyleHeader}>My flights</Text>
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <CardFlight
-              data={{
-                departure: {
-                  country: 'Serbia',
-                  countryCode: 'BEG',
-                },
-                arrival: {
-                  country: 'Netherlands',
-                  countryCode: 'AMS',
-                },
-                date: 'September 3, 2020',
-                passengers: '2 passengers',
-              }}
-            />
-            <CardFlight
-              data={{
-                departure: {
-                  country: 'Serbia',
-                  countryCode: 'BEG',
-                },
-                arrival: {
-                  country: 'Netherlands',
-                  countryCode: 'AMS',
-                },
-                date: 'September 3, 2020',
-                passengers: '2 passengers',
-              }}
-            />
-            <CardFlight
-              data={{
-                departure: {
-                  country: 'Serbia',
-                  countryCode: 'BEG',
-                },
-                arrival: {
-                  country: 'Netherlands',
-                  countryCode: 'AMS',
-                },
-                date: 'September 3, 2020',
-                passengers: '2 passengers',
-              }}
-            />
-            <CardFlight
-              data={{
-                departure: {
-                  country: 'Serbia',
-                  countryCode: 'BEG',
-                },
-                arrival: {
-                  country: 'Netherlands',
-                  countryCode: 'AMS',
-                },
-                date: 'September 3, 2020',
-                passengers: '2 passengers',
-              }}
-            />
-            <CardFlight
-              data={{
-                departure: {
-                  country: 'Serbia',
-                  countryCode: 'BEG',
-                },
-                arrival: {
-                  country: 'Netherlands',
-                  countryCode: 'AMS',
-                },
-                date: 'September 3, 2020',
-                passengers: '2 passengers',
-              }}
-            />
-            <CardFlight
-              data={{
-                departure: {
-                  country: 'Serbia',
-                  countryCode: 'BEG',
-                },
-                arrival: {
-                  country: 'Netherlands',
-                  countryCode: 'AMS',
-                },
-                date: 'September 3, 2020',
-                passengers: '2 passengers',
-              }}
-            />
-          </ScrollView>
-          <FloatingButton />
+          <FlatList
+            data={this.state.info}
+            keyExtractor={(item) => item.id}
+            extraData={this.state.info}
+            renderItem={({ item }) => <CardFlight data={item.data} />}
+          />
+
+          <FloatingButton navigate={this.props.navigation} />
         </View>
       </View>
     );
